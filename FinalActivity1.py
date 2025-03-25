b1b3e4e96f0b58333ebb7b204ec363e15dd65c58
Allen Jefferson <94884406+AJOrcino@users.noreply.github.com>
2025-03-25 09:08:46 +0800

    Using Python

---
+++ FinalActivity1.py
@@ -0,0 +1,8 @@
+def SortedList(strings):
+    return sorted(set(strings))
+
+InputList = ["Python", "Java", "C++", "C#", "C", "JavaScript", "Ruby", "Go", "Swift", 
+            "PHP", "Perl"]
+OutputList = SortedList(InputList)
+print(OutputList)
+print("Allen Jefferson C. Orcino")
